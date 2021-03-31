190c61662a1ba3acfa201d3d40969a9df728e4c7
omondi.edgar@yahoo.com <omondi.edgar@yahoo.com>
2021-03-31 11:35:21 +0300

    RemoteWebDriver Docker

--- src/main/java/setup/Base.java
+++ src/main/java/setup/Base.java
@@ -3,6 +3,7 @@ package setup;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -15,43 +16,53 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Base {
 	public static WebDriver driver;
+	// public static RemoteWebDriver driver;
 	public Properties prop;
-	public WebDriver initializeDriver () throws IOException {
+
+	public WebDriver initializeDriver() throws IOException {
 		prop = new Properties();
-		FileInputStream fis = new  FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\setup\\global.properties");
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\main\\java\\setup\\global.properties");
 		prop.load(fis);
-		
-		String browserName=prop.getProperty("browser");
+
+		String browserName = prop.getProperty("browser");
 		String currentDir = System.getProperty("user.dir");
-		if(browserName.equals("chrome")) {			
-			System.setProperty("webdriver.chrome.driver", currentDir + "\\src\\chromedriver89.exe");			
-			ChromeOptions options = new ChromeOptions();		  
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", currentDir + "\\src\\chromedriver89.exe");
+			ChromeOptions options = new ChromeOptions();
 			options.addArguments("enable-automation");
-			options.addArguments("--headless");		
+			options.addArguments("--headless");
 			driver = new ChromeDriver(options);
-			
-		} else if(browserName.contentEquals("firefox")) {
+
+			/*
+			 * URL url = new URL("http://localhost:4444/wd/hub"); DesiredCapabilities dc =
+			 * DesiredCapabilities.chrome(); driver = new RemoteWebDriver(url, dc);
+			 */
+
+		} else if (browserName.contentEquals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", currentDir + "\\src\\geckodriver.exe");
 			FirefoxOptions options = new FirefoxOptions();
 			options.addArguments("enable-automation");
 			options.addArguments("--headless");
 			driver = new FirefoxDriver(options);
-			
+
 		} else if (browserName.contentEquals("internetexplorer")) {
-			System.setProperty("webdriver.ie.driver", currentDir + "\\src\\IEDriverServer.exe");			
+			System.setProperty("webdriver.ie.driver", currentDir + "\\src\\IEDriverServer.exe");
 			driver = new InternetExplorerDriver();
-			
-		}		
+
+		}
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		return driver;
 	}
-	
+
 	public void getScreenshot(String result) throws IOException {
-		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(src, new File("C:\\Users\\eonyango\\eclipse-workspace\\Herokuapp\\src\\screenshots\\"+result+".png"));
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File(System.getProperty("user.dir") + "\\src\\screenshots\\" + result + ".png"));
 	}
 
 }
